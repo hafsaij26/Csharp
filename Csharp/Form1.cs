04346af6042ca5beddd853daaf28dc598d4c0f0a
Hafsa Ijaz <hafsaijaz0206@gmail.com>
2026-06-15 00:38:00 +0500

    System 2

---
+++ Csharp/Form1.cs
@@ -0,0 +1,56 @@
+namespace sys2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    "Please select course first!",
+                    "Warning",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                    );
+
+            }
+            else
+            {
+                label2.Text = comboBox1.SelectedItem.ToString();
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            comboBox1.Items.Add("Programming Fundamentals");
+            comboBox1.Items.Add("Artificial Intelligence");
+            comboBox1.Items.Add("Computer Network");
+            comboBox1.Items.Add("Database System");
+        }
+    }
+}
